5c987ba461040065a1ffdd853041b36d981c81c6
郭宇航 <1131717341@qq.com>
2021-08-16 18:31:45 +0800

    复习spring-改造普通结构使用IOC托管

--- spring-01-ioc/src/test/java/MyTest.java
+++ spring-01-ioc/src/test/java/MyTest.java
@@ -1,10 +1,14 @@
 import com.kuang.dao.UserDaoMysqlImpl;
 import com.kuang.service.UserService;
 import com.kuang.service.UserServiceImpl;
+import javafx.application.Application;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
 
     public static void main(String[] args) {
+        /*
         //用户实际是调用的业务层，dao层他们不用接触！
         UserService userService = new UserServiceImpl();
 
@@ -12,5 +16,11 @@ public class MyTest {
         userService.setUserDao(new UserDaoMysqlImpl());
 
         userService.getUser();
+        */
+
+        //引用spring容器创建好的对象
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        UserService service = (UserService) context.getBean("service");
+        service.getUser();
     }
 }
